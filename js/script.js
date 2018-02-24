6c13fec5c0288e422db68797661647854ccb7564
ZacKnights <35971223+ZacKnights@users.noreply.github.com>
2018-02-24 13:47:56 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,18 +8,25 @@ function currentYear(){
   autoDate.textContent = date.getFullYear();
 };
 
-window.onscroll = function() {
-  headerScroll();
-}
+var feed = new Instafeed({
+  get: 'user',
+  userId: '2297561537',
+  accessToken: '2297561537.1677ed0.84731cdddd524f1987d5ef449ca437f6',
+  filter: function(image) {
+  var MAX_LENGTH = 40;
 
-var header = document.getElementById("header");
-
-var stick = header.offsetTop;
-
-function headerScroll(){
-  if (window.pageYOffset >= stick) {
-    header.classList.add("stick");
+  // here we create a property called "short_caption"
+  // on the image object, using the original caption
+  if (image.caption && image.caption.text) {
+    image.short_caption = image.caption.text.slice(0, MAX_LENGTH);
   } else {
-    header.classList.remove("stick");
+    image.short_caption = "";
   }
-}
+
+  // ensure the filter doesn't reject any images
+  return true;
+},
+  template: '<div class="col-2"><img class="w-100" src={{image}}><hr><p class="likes"><i class="fas fa-heart"></i>{{likes}}<p>{{model.short_caption}}...<a href="{{link}}" target="_blank">Read More</a></p></div>'
+});
+
+feed.run();
